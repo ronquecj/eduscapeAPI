e347e3ee2bf723c8bd223dab9a5b1d0c8bf079b1
christian <ronquechristian@gmail.com>
2024-06-17 19:11:48 +0800

    isadmim

--- models/user.model.js
+++ models/user.model.js
@@ -69,6 +69,10 @@ const userSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
