2630fcc82759cf3b7c75932acd63bf003e652fdf
christian <ronquechristian@gmail.com>
2024-06-18 19:33:16 +0800

    minor changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -75,6 +75,7 @@ export const uploadGcashQR = async (req, res, next) => {
 
 export const getAdminData = async (req, res, next) => {
   try {
+    const { id } = req.params;
     const user = await User.findById(id);
 
     res.status(200).json(user);
